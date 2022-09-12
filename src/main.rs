91db02500e0422993e67ed04530a80450be955a6
patrickbindelli <patrickvenanbindelli@gmail.com>
2022-09-11 23:33:37 -0300

    added vga buffer

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
     loop {}
 }
